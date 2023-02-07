25b35716b6642557fdff5c901baea044049ae72b
Сергей Гришин <dr_grishin_91@mail.ru>
2023-02-07 09:06:19 +0300

    update поправил форму создания финансов

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,7 +16,6 @@ namespace myPet4.Controllers
 {
     public class HomeController : Controller
     {
-        //public static Persons currentPerson;
         public static UserData currentUser = new UserData();
         private readonly myPetContext _context;
         public HomeController(myPetContext context)
@@ -44,7 +43,6 @@ namespace myPet4.Controllers
                 if (persons.IsNullOrEmpty())
                 {
                     Persons person = new Persons(login);
-                    //currentPerson = person;
                     currentUser.Person = person;
                     return View("createFinance");
                 }
@@ -66,7 +64,6 @@ namespace myPet4.Controllers
 
         [HttpPost]
         public IActionResult createFinance(int ID, int cash, int credit, int toSave, int salary, DateTime dateBegin, DateTime? dateEnd, string step)
-        //public IActionResult createFinance(string step)
         {
             DateTime d2 = dateBegin;
             char s = 'c';
@@ -96,7 +93,6 @@ namespace myPet4.Controllers
 
             if (ModelState.IsValid)
             {
-                ////currentPerson.Finance = new Finance(currentPerson.id, cash, credit, toSave, salary, dateBegin, d2, s);
                 currentUser.Person.Finance = new Finance(currentUser.Person.id, cash, credit, toSave, salary, dateBegin, d2, s);
 
                 return View("CreateItems");
@@ -139,7 +135,6 @@ namespace myPet4.Controllers
 
         public IActionResult UserForm()
         {
-            //UserData d = new UserData(currentPerson);
             currentUser = new UserData(currentUser.Person);
             return View("UserForm", currentUser);
         }
@@ -147,26 +142,18 @@ namespace myPet4.Controllers
         [HttpGet]
         public IActionResult CreateItems()
         {
-            //if (currentUser.Person.ItemPerson.IsNullOrEmpty()) currentUser.Person.ItemPerson = new List<ItemPerson>();
-            //currentUser.Person.ItemPerson.Add(new ItemPerson());
             return View("CreateItems");
         }
 
         [HttpPost]
         public IActionResult CreateItems(string nameof, int summ)
         {
-            ////currentPerson.ItemPerson.Add(new ItemPerson(currentPerson.id, nameof, summ));
-            //currentUser.Person.ItemPerson.Add(new ItemPerson(currentUser.Person.id, nameof, summ));
             currentUser.Person.ItemPerson.Add(new ItemPerson(currentUser.Person.id, nameof, summ));
             return RedirectToAction();
         }
 
         public IActionResult saveProfile()
         {
-            //_context.Add(currentPerson);
-            //_context.Finance.Add(currentPerson.Finance);
-            //_context.Item.Add(currentPerson.ItemPerson.Last());
-
             _context.Add(currentUser.Person);
             _context.Finance.Add(currentUser.Person.Finance);
             _context.Item.Add(currentUser.Person.ItemPerson.Last());
